29f009a2aaadf818d3824dfe5bd979599131b761
Nick Anderson Azemar <newnick1996@gmail.com>
2021-12-21 13:23:21 -0500

    .

--- Pages/MessageWall.cshtml.cs
+++ Pages/MessageWall.cshtml.cs
@@ -11,12 +11,24 @@ namespace RazorMessageWallApp.Pages
     public class MessageWallModel : PageModel
     {
         private readonly ILogger<MessageWallModel> _logger;
+        [BindProperty] //Mean that you can post to this
+        public string Message { get; set; }
+
+        [BindProperty]
+        public List<string> Messages { get; set; } = new List<string>();
         public MessageWallModel(ILogger<MessageWallModel> logger)
         {
             _logger = logger;
         }
         public void OnGet()
         {
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            Messages.Add(Message);
+            return Page();
         }
     }
 }
